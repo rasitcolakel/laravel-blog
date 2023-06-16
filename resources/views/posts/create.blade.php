2adccc10fc8554ba3894bc934d5e903b4fecbc0c
Raşit Çolakel <rasitcolakel@hotmail.com>
2023-06-16 23:53:18 +0300

    layout applied to create and index pages of posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,32 +1,25 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1>Posts</h1>
-<form method="post" action="{{route("posts.store")}}">
-    @csrf
-    @method("post")
+@extends('layout')
 
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <div>{{$error}}</div>
-        @endforeach
-    @endif
+@section('title', 'Create Post')
 
-    <label>
-        <input type="text" name="title" placeholder="Title" />
-    </label>
-    <label>
-        <input type="text" name="description" placeholder="description" />
-    </label>
+@section('content')
+    <form method="post" action="{{route("posts.store")}}">
+        @csrf
+        @method("post")
 
-    <input type="submit" value="Create" />
-</form>
-</body>
-</html>
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <div>{{$error}}</div>
+            @endforeach
+        @endif
+
+        <label>
+            <input type="text" name="title" placeholder="Title" />
+        </label>
+        <label>
+            <input type="text" name="description" placeholder="description" />
+        </label>
+
+        <input type="submit" value="Create" />
+    </form>
+@endsection
